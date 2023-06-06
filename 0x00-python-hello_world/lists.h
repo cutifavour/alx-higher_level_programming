39bd00ed3d6c4c75cf42188803d7d759d16384de
cutrifavour <cuti.favour@yahoo.com>
2023-06-05 21:07:08 -0700

    my first python project is done

---
+++ 0x00-python-hello_world/lists.h
@@ -0,0 +1,25 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <stdlib.h>
+
+/**
+ * struct listint_s - this is singly linked list
+ * @n: integer now
+ * @next:this points to the next node
+ *
+ * Description:this is singly linked list node structure
+ * for Holberton project
+ */
+typedef struct listint_s
+{
+	int n;
+	struct listint_s *next;
+} listint_t;
+
+size_t print_listint(const listint_t *h);
+int check_cycle(listint_ *list);
+listint_t *add_nodeint(listint_t **head, const int n);
+void free_listint(listint_t *head);
+
+#endif /* LISTS_H */

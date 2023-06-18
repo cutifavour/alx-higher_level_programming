fb6e9fac02ee50b244a2ff1f36c816fee5760583
cutrifavour <cuti.favour@yahoo.com>
2023-06-18 06:48:11 -0700

    my python import and modules correction

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,7 +4,7 @@
          """this print the number of and list of arguments."""
              import sys
 
-                 count = len(sys.argv) - 1
+                 count = len("sys.argv") - 1
                      if count == 0:
                                  print("0 arguments.")
                                      elif count == 1:
